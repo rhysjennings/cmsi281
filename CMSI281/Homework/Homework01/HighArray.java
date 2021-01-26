80acdc0264d01e4d5eaa70d019e7be78df0d6390
Rhys Jennings <rhysnjennings@gmail.com>
2021-01-26 13:34:35 -0800

    Uploading Homeworks

--- CMSI281/Homework/Homework01/HighArray.java
+++ CMSI281/Homework/Homework01/HighArray.java
@@ -1,29 +1,87 @@
-class HighArrayApp
-{
-public static void main(String[] args)
-{
-int maxSize = 100; // array size
-HighArray arr; // reference to array
-arr = new HighArray(maxSize); // create the array
-arr.insert(77); // insert 10 items
-arr.insert(99);
-arr.insert(44);
-arr.insert(55);
-arr.insert(22);
-arr.insert(88);
-arr.insert(11);
-arr.insert(00);
-arr.insert(66);
-arr.insert(33);
-arr.display(); // display items
-int searchKey = 35; // search for item
-if( arr.find(searchKey) )
-System.out.println(“Found “ + searchKey);
-else
-System.out.println(“Can’t find “ + searchKey);
-arr.delete(00); // delete 3 items
-arr.delete(55);
-arr.delete(99);
-arr.display(); // display items again
-} // end main()
-} // end class HighArrayApp
+// highArray.java
+// demonstrates array class with high-level interface
+// to run this program: C>java HighArrayApp
+////////////////////////////////////////////////////////////////
+import java.util.*;
+//import java.lang.*;
+
+class HighArray
+  {
+  private long[] a; // ref to array a
+  private int nElems; // number of data items
+  private int max;
+
+//-----------------------------------------------------------
+  public HighArray(int max) // constructor
+    {
+    a = new long[max]; // create the array
+    nElems = 0; // no items yet
+    }
+//-----------------------------------------------------------
+  public boolean find(long searchKey)
+    { // find specified value
+    int j;
+    for(j = 0; j < nElems; j++) // for each element,
+      if(a[j] == searchKey) // found item?
+          break; // exit loop before end
+      if(j == nElems) // gone to end?
+        return false; // yes, can’t find it
+      else
+        return true; // no, found it
+      } // end find()
+//-----------------------------------------------------------
+  public void insert(long value) // put element into array
+    {
+    a[nElems] = value; // insert it
+    nElems++; // increment size
+  }
+//-----------------------------------------------------------
+  public boolean delete(long value) {
+    int j;
+    for(j = 0; j < nElems - 1; j++) // look for it
+      if( value == a[j] )
+        break;
+      if(j == nElems) // can’t find it
+        return false;
+      else { // found it
+        for(int k = j; k<nElems; k++) // move higher ones down
+          a[k] = a[k + 1];
+        nElems--; // decrement size
+        return true;
+      }
+    } // end delete()
+//-----------------------------------------------------------
+  public void display() { // displays array contents
+    for(int j = 0; j < nElems; j++) // for each element,
+      System.out.print(a[j] + " "); // display it
+      System.out.println("");
+  }
+//-----------------------------------------------------------
+  public void getMax() { //returns the value of the highest key in the array
+    max = 0;
+    for (int i = 1; i < a.length; i++) {
+      if(a.length == 0) {
+        max = -1;
+      }
+      else if (a[i] > max){
+        max = (int)a[i];
+      }
+    } System.out.println("This is the max: "+max);
+  }
+//-----------------------------------------------------------
+  public void noDupes() { // Removes the duplicates in an Array and reassigns them back to a
+      Set<Long> b = new HashSet<Long>();
+      for( long l : a ) {
+         b.add( new Long( l ) );
+      }
+      a = new long[a.length];
+      int i = 0;
+      for( Long l : b ) {
+         a[i++] = l.longValue();
+      }
+      nElems = b.size();
+      System.out.println("Currently removing the Dupes!");
+   }
+
+  }
+
